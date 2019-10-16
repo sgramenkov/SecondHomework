e24282d45ec46fedac69c6d912ffa08d59f6b06d
sgramenkov <56446832+sgramenkov@users.noreply.github.com>
2019-10-16 19:44:52 +0300

    final

---
+++ app/src/main/java/bonch/dev/school/SecondFragment.kt
@@ -0,0 +1,18 @@
+package bonch.dev.school
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SecondFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_second,container,false)
+        return view
+    }
+}
